845b9ea34276ee6c5b1966b9e1183cf39839c389
dummmylitt <changauds@gmail.com>
2023-01-20 14:04:26 -0800

    Exercise 4

---
+++ arduino/exericse_4.ino
@@ -0,0 +1,118 @@
+void setup() {
+  // put your setup code here, to run once:
+  //connect pins to 7 segment display 
+
+  Serial.begin(9600);
+  
+  pinMode(13, OUTPUT);
+  pinMode(12 , OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //loop 9-0 
+    //9
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+    delay(1500); 
+    //8
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW); 
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+    delay(1500);
+    //7
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(7, HIGH);
+    delay(1500);
+
+    //6
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+    delay(1500);
+
+    //5
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+    delay(1500);
+
+    //4
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, LOW);
+    digitalWrite(7, LOW);
+        delay(1500);
+
+    //3
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(7, LOW);
+        delay(1500);
+
+    //2
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, HIGH);
+    digitalWrite(7, LOW);
+    delay(1500);
+
+    //1
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(7, HIGH);
+    delay(1500);
+
+    //0
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(7, HIGH);
+    delay(1500);
+
+  
+}
